1eaaf71ad96a104c374b3bee3be5c5ab4d41aa85
SKrishnaMJ <146481712+SKrishnaMJ@users.noreply.github.com>
2025-01-03 11:16:06 -0800

    fifth commit consisting of CreateView class

--- reviews/views.py
+++ reviews/views.py
@@ -1,33 +1,21 @@
 from typing import Any
 from django.db.models.query import QuerySet
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 from .forms import ReviewForm
 from django.views import View
 from django.views.generic.base import TemplateView
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import FormView, CreateView
 from .models import Review
 
 # Create your views here.
-class ReviewView(View):
-    def get(self, request):
-        form = ReviewForm()
-        return render(request, "reviews/review.html", {
-        "form":form})
+class ReviewView(CreateView):
+    model = Review
+    form_class = ReviewForm
+    template_name = "reviews/review.html"
+    success_url = "/thank-you"
     
-    def post(self, request):
-        form = ReviewForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect("/thank-you")
-        form = ReviewForm()
-        return render(request, "reviews/review.html", {
-        "form":form})
-
-
-# class ThankYouView(View):
-#     def get(self, request):
-#         return render(request, "reviews/thankyou.html")
     
 class ThankYouView(TemplateView):
     template_name = "reviews/thankyou.html"
@@ -44,7 +32,7 @@ class ReviewList(ListView):
 
     def get_queryset(self):
         base_query = super().get_queryset()
-        data = base_query.filter(rating__gt=4)
+        data = base_query.filter(rating__gt=3)
         return data
 
     
