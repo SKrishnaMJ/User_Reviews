1e97dec153ecfd7c99d4f04bb47a317b4c43b97e
SKrishnaMJ <146481712+SKrishnaMJ@users.noreply.github.com>
2025-01-03 10:47:06 -0800

    fourth commit consisting of different View classes

--- reviews/views.py
+++ reviews/views.py
@@ -1,7 +1,12 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from .forms import ReviewForm
 from django.views import View
+from django.views.generic.base import TemplateView
+from django.views.generic import ListView, DetailView
+from .models import Review
 
 # Create your views here.
 class ReviewView(View):
@@ -19,5 +24,32 @@ class ReviewView(View):
         return render(request, "reviews/review.html", {
         "form":form})
 
-def thank_you(request):
-    return render(request, "reviews/thankyou.html")
+
+# class ThankYouView(View):
+#     def get(self, request):
+#         return render(request, "reviews/thankyou.html")
+    
+class ThankYouView(TemplateView):
+    template_name = "reviews/thankyou.html"
+
+    def get_context_data(self, **kwargs):
+        content = super().get_context_data(**kwargs)
+        content["message"] = "This works!"
+        return content
+    
+class ReviewList(ListView):
+    template_name = "reviews/reviews.html"
+    model = Review
+    context_object_name = "reviews"
+
+    def get_queryset(self):
+        base_query = super().get_queryset()
+        data = base_query.filter(rating__gt=4)
+        return data
+
+    
+    
+class SingleReview(DetailView):
+    template_name = "reviews/single_review.html"
+    model = Review
+        
